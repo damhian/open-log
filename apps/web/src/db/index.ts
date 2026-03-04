538734b0e1b25f306fc89850f388129af21db801
damhian <damhian>
2026-03-04 21:16:01 +0700

    Build-Time Fallback

--- apps/web/src/db/index.ts
+++ apps/web/src/db/index.ts
@@ -2,5 +2,7 @@ import { neon } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
 
 // The ! indicates that we assert DATABASE_URL is provided in .env.local
-const sql = neon(process.env.DATABASE_URL!);
+const connectionString = process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost/dummy';
+
+const sql = neon(connectionString);
 export const db = drizzle({ client: sql });
